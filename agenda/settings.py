5c7dd1d3773b93c75458f5eb775dcfecdfdc6f70
luissilv4 <luisandremarreirossilva@gmail.com>
2020-04-13 18:49:50 +0100

    fixed static_dir

--- agenda/settings.py
+++ agenda/settings.py
@@ -153,7 +153,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
+STATIC_DIR = os.path.join(BASE_DIR,"static")
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 
 #REST REST_FRAMEWORK
