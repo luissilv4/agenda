5a64c4d2d4aaf172dbd07f088e46b8758bb2b47a
luissilv4 <luisandremarreirossilva@gmail.com>
2020-05-05 00:08:46 +0100

    added search functionality

--- clients/api/viewsets.py
+++ clients/api/viewsets.py
@@ -5,11 +5,20 @@ from rest_framework.decorators import action
 from rest_framework import viewsets
 from .serializers import (ClientSerializer, )
 from clients.models import Client
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
+
+
+
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 1000
+
+
+
+
 
 class ClientViewSet(viewsets.ModelViewSet):
     """
@@ -21,28 +30,10 @@ class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
     permission_classes = (IsAuthenticated, )
-    pagination_class = StandardResultsSetPagination
-    # filter_backends = (DjangoFilterBackend, OrderingFilter,)
+    # pagination_class = StandardResultsSetPagination
+    filter_backends = (DjangoFilterBackend, OrderingFilter, SearchFilter)
+    search_fields = ('name', 'phone_number', 'id', 'cc', 'nif')
+    ordering_fields = ('id','name')
+    ordering = ('name')
     # detail_serializer_class = AppointmentDetailSerializer
     lookup_field = 'uuid'
-
-    # def get_serializer_class(self):
-    #     if self.action == 'list':
-    #         return self.list_serializer
-    #     return self.serializer_class
-
-
-    # def get_queryset(self):
-    #         """
-    #         Optionally restricts the returned queries by filtering against
-    #         a `sport` and `name` query parameter in the URL.
-    #         """
-    #         queryset = Match.objects.all()
-    #         sport = self.request.query_params.get('sport', None)
-    #         name = self.request.query_params.get('name', None)
-    #         if sport is not None:
-    #             sport = sport.title()
-    #             queryset = queryset.filter(sport__name=sport)
-    #         if name is not None:
-    #             queryset = queryset.filter(name=name)
-    #         return queryset
