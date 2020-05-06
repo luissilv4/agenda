e682f2478f9bca0b98d4d000a2146c1debba2b75
Luis Silva <luisandremarreirossilva@gmail.com>
2020-05-06 21:35:58 +0100

    added offices

--- services/api/urls.py
+++ services/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from .views import (ServiceListCreateView, ServiceRetrieveUpdateDestroyAPIView,
-                    )
+                    OfficeListCreateView, OfficeRetrieveUpdateDestroyAPIView)
 
 urlpatterns = [
     path('', ServiceListCreateView.as_view()),
+    path('offices/', OfficeListCreateView.as_view()),
+    path('offices/<pk>', ServiceRetrieveUpdateDestroyAPIView.as_view()),
     path('<pk>', ServiceRetrieveUpdateDestroyAPIView.as_view()),
-    # path('offices/', OfficeListCreateView.as_view())
 ]
