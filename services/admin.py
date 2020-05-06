e1d4eccfd108bd52d3a2d342cd516a8f5c377e74
Luis Silva <luisandremarreirossilva@gmail.com>
2020-05-06 21:02:47 +0100

    added office model

--- services/admin.py
+++ services/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from services.models import Service
+from services.models import Service, Office
 
 # Register your models here.
 
 admin.site.register(Service)
+admin.site.register(Office)
--- services/models.py
+++ services/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+
+class Office(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
 class Service(models.Model):
     name = models.CharField(max_length=75,unique=True)
     description = models.CharField(max_length=150, null=True, blank=True)
     duration = models.TimeField()
-    price = models.IntegerField()
-
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return self.name
